270d4cdf4a66f083019c0f364f0a693b0746c1e6
Avonavi23 <anna230997@mail.ru>
2021-08-30 23:19:17 +0400

    feat: add brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const bracketsСhars = bracketsConfig
+    .reduce((acc, item) => (acc += item), '')
+    .split('')
+    .filter((item) => item !== ',')
+    .join('');
+  
+  const arrStr = str.split('');
+  const stack = [];
+  
+  arrStr.forEach((char) => {
+    const findedCharIndex = bracketsСhars.indexOf(char);
+    const hasChar = findedCharIndex !== -1;
+  
+    if (!hasChar) {
+      return false;
+    }
+  
+    if (findedCharIndex % 2 > 0) {
+      if (stack.length === 0) {
+        return false;
+      }
+      
+      const lastBracket = stack.pop();
+      
+      if (lastBracket !== bracketsСhars[findedCharIndex - 1]) {
+        return false;
+      }
+    }
+  
+    if (findedCharIndex % 2 === 0) {
+      stack.push(char);
+    }
+  })
+  
+  if (str[0] === '|') {
+    return true;
+  }
+  
+  return stack.length === 0;
 }
+  
